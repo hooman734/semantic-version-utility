dc2da8778360308c860fdc1e49f77d0c84cf3884
Hooman <hesamyan@outlook.com>
2020-06-13 10:03:23 +0400

    fixing

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def next_version_json(package_name, type):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- wsgi.py
+++
@@ -1,3 +0,0 @@
-from flask import create_app
-
-app = create_app()
